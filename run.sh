7629ad587033d9c79ba4cc3e1346e4010195f09b
toggle1995 <huyutao@buaa.edu.cn>
2022-11-17 19:50:10 +0800

    debug

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ cp ./run.sh /mnt/lustre/huyutao.vendor/record/lavit/h11
 cp ./*.py /mnt/lustre/huyutao.vendor/record/lavit/h11
 cp -r ./lib/ /mnt/lustre/huyutao.vendor/record/lavit/h11
 srun --partition=sensedeep  --mpi=pmi2 --gres=gpu:4 -N 1  --job-name=SEG_h11 --kill-on-bad-exit=1 --pty --mail-type=ALL \
-python -m torch.distributed.launch --nproc_per_node 4 --master_port 12364 train_flip.py --model lavt --dataset refcoco \
+python -m torch.distributed.launch --nproc_per_node 4 --master_port 12365 train_flip.py --model lavt --dataset refcoco \
 --model_id h11 --output-dir /mnt/lustre/huyutao.vendor/record/lavit/h11/ --batch-size 8 --lr 0.00005 --wd 1e-2 --swin_type base --pretrained_swin_weights ./pretrained_weights/swin_base_patch4_window12_384_22k.pth \
 --epochs 60 --img_size 480 2>&1 | tee /mnt/lustre/huyutao.vendor/record/lavit/h11/output
 # -w SH-IDC1-10-198-6-53
