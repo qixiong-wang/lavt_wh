28d355c19d6e54e2548dba9fb59c88e1d8a9b5f2
wangqx <wqx1140@buaa.edu.cn>
2022-12-11 21:34:50 -0500

    debug

--- train.py
+++ train.py
@@ -251,6 +251,7 @@ def main(args):
             {'params': backbone_no_decay, 'weight_decay': 0.0},
             {'params': backbone_decay},
             {"params": [p for p in single_model.classifier.parameters() if p.requires_grad]},
+            {"params": [p for p in single_model.refinement.parameters() if p.requires_grad]},
             # the following are the parameters of bert
             {"params": reduce(operator.concat,
                               [[p for p in single_bert_model.encoder.layer[i].parameters()
