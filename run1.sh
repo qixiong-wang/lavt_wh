b200250ed3057fec09275287ce4bb7672c4ced3c
toggle1995 <huyutao@buaa.edu.cn>
2022-12-24 10:46:21 +0800

    h57_1224

--- run1.sh
+++ run1.sh
@@ -1,15 +1,15 @@
 # export CUDA_VISIBLE_DEVICES=0,1,2,3
-mkdir /mnt/petrelfs/huyutao/record/lavit_hu1/h55_1223
-cp ./run1.sh /mnt/petrelfs/huyutao/record/lavit_hu1/h55_1223
-cp ./*.py /mnt/petrelfs/huyutao/record/lavit_hu1/h55_1223
-cp -r ./data /mnt/petrelfs/huyutao/record/lavit_hu1/h55_1223
-cp -r ./lib /mnt/petrelfs/huyutao/record/lavit_hu1/h55_1223
-srun --partition=Gvadapt  --mpi=pmi2 --gres=gpu:4 -N 1  --job-name=h55_1223 --kill-on-bad-exit=1 --quotatype=auto --async -o /mnt/petrelfs/huyutao/record/lavit_hu1/h55_1223/log.out --pty --mail-type=ALL \
-python -m torch.distributed.launch --nproc_per_node 4 --master_port 18709 train_conloss.py --model lavt --dataset refcoco+ \
---model_id h55_1223 --output-dir /mnt/petrelfs/huyutao/record/lavit_hu1/h55_1223 --batch-size 8 --lr 0.00005 --wd 1e-2 --swin_type base --pretrained_swin_weights ~/pretrained_model/swin_base_patch4_window12_384_22k.pth \
---epochs 60 --rootpath /mnt/petrelfs/huyutao/record/lavit_hu1/ --img_size 480 --tar_size 480 2>&1 | tee /mnt/petrelfs/huyutao/record/lavit_hu1/h55_1223/output
+mkdir /mnt/petrelfs/huyutao/record/lavit_hu1/h57_1224
+cp ./run1.sh /mnt/petrelfs/huyutao/record/lavit_hu1/h57_1224
+cp ./*.py /mnt/petrelfs/huyutao/record/lavit_hu1/h57_1224
+cp -r ./data /mnt/petrelfs/huyutao/record/lavit_hu1/h57_1224
+cp -r ./lib /mnt/petrelfs/huyutao/record/lavit_hu1/h57_1224
+srun --partition=Gvadapt  --mpi=pmi2 --gres=gpu:4 -N 1  --job-name=h57_1224 --kill-on-bad-exit=1 --quotatype=auto --async -o /mnt/petrelfs/huyutao/record/lavit_hu1/h57_1224/log.out --pty --mail-type=ALL \
+python -m torch.distributed.launch --nproc_per_node 4 --master_port 18718 train_conloss.py --model lavt --dataset refcoco \
+--model_id h57_1224 --output-dir /mnt/petrelfs/huyutao/record/lavit_hu1/h57_1224 --batch-size 8 --lr 0.00005 --wd 1e-2 --swin_type base --pretrained_swin_weights ~/pretrained_model/swin_base_patch4_window12_384_22k.pth \
+--epochs 60 --rootpath /mnt/petrelfs/huyutao/record/lavit_hu1/ --img_size 480 --tar_size 480 2>&1 | tee /mnt/petrelfs/huyutao/record/lavit_hu1/h57_1224/output
 # -w SH-IDC1-10-198-6-53
-#--async -o /mnt/petrelfs/huyutao/record/lavit_hu1/h55_1223/log.out
+#--async -o /mnt/petrelfs/huyutao/record/lavit_hu1/h57_1224/log.out
 #~/pretrained_model/swinv2_base_patch4_window12_192_22k.pth
 #~/pretrained_model/lavt/ref_refcocogoogle_window12.pth
 #~/pretrained_model/swin_base_patch4_window12_384_22k.pth
